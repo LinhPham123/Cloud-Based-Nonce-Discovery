1d3f2c29a5db257a621b9e0e75e11bee496c6439
LinhPham123 <33656362+LinhPham123@users.noreply.github.com>
2019-12-06 04:37:32 +0000

    emergency stop file

---
+++ scram.py
@@ -0,0 +1,15 @@
+import boto3
+
+if __name__ == "__main__":
+    print("Emergency closing everything on Cloud")
+
+    ec2_resource = boto3.resource('ec2')
+    ec2_resource.instances.terminate()
+
+    bucket_to_kill = input("Enter the bucket name you wish to kill (n/a if not): ")
+    if bucket_to_kill != 'n/a':
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_to_kill)
+        for item in bucket.objects.all():s
+            item.delete()
+        s3.Bucket(bucket_to_kill).delete()
